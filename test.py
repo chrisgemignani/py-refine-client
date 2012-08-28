48efc2873fa259d9ae09be390d73f73c3fe045c4
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2012-08-28 12:12:25 -0400

    Organize the tests into test cases suitable for nose tests
    To run 
      
    $ nosetests

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from refine_server import Facet, ListFacet, TextFacet, TimeRangeFacet, RangeFacet, Project, RefineServer
 
-def test():
-    pass
+
 
 def testServerCapabilities():
     """
@@ -9,87 +8,98 @@ def testServerCapabilities():
     :return:
     """
     try:
-        s=RefineServer()
+        s = RefineServer()
     except Exception, e:
         raise
 
     assert s.version.full_name == 'Google Refine 2.5 [r2407]'
     server_configuration = s.configuration
-    assert 'binary/xls' in server_configuration.formats
+    assert 'binary/xls' in [f.name for f in server_configuration.formats]
     assert 'application/vnd.ms-excel' in server_configuration.mime_types.keys()
     assert server_configuration.mime_types['application/vnd.ms-excel'] == 'binary/xls'
 
-#    print "\nThe server is running {0}\n".format(s.version.full_name)
 
-s=RefineServer()
+def testServerUpload():
+    """
+    Testing project creation using media.juiceanalytics.com CSV API...
+    :return:
+    """
+    try:
+        s = RefineServer()
+        num_projects = len(s.projects)
+        p = Project(url='http://demo.mediacore.tv/api/media?category=featured', name='testing', **{'recordPath':["__anonymous__","media","__anonymous__"]})
+        p.transform_column("__anonymous__ - modified_on", "value.toDate()")
+        comps = p.test_facets([TimeRangeFacet("__anonymous__ - modified_on", "__anonymous__ - modified_on", -6470126652719022000, -2305843009213694000)])
+        assert len(s.projects) == (num_projects + 1)
+        assert len(comps) == 1
+    except Exception, e:
+        raise
+    finally:
+        p.destroy()
 
-project_list = s.projects
-
-print "There are currently {0} projects:\n".format(len(project_list))
-for p in project_list:
-    print p.id
-server_configuration = s.configuration
-print "--Server Configuration--\n\nFormats:\n"
-for f in server_configuration.formats:
-    print f.name
-print "MIME Types:\n"
-print server_configuration.mime_types
-print "File Extensions:\n"
-print server_configuration.file_extensions
-print "Testing project creation using demo.mediacore.tv JSON API..."
-print "\nThe server is running {0}\n".format(s.version.full_name)
 
 
-#try:
-#    p=Project(url='http://demo.mediacore.tv/api/media?category=featured', name='testing', **{'recordPath':["__anonymous__","media","__anonymous__"]})
-#    p.transform_column("__anonymous__ - modified_on", "value.toDate()")
-#    comps = p.test_facets([TimeRangeFacet("__anonymous__ - modified_on", "__anonymous__ - modified_on", -6470126652719022000, -2305843009213694000)])
-#    print comps
-#    p.destroy()
-#except Exception, e: raise
-#print "Testing project creation using media.juiceanalytics.com CSV API..."
-#try:
-#    p2=Project(url='http://media.juiceanalytics.com/census/CENSUS_STATEAGESEX.csv', name="census")
-#    print "Columns comprise:\n"
-#    for o in p2.columns: print "{0}: {1}".format(o.cell_index, o.name)
-#    p2.split_column_by_separator(p2.column_names[0], ",")
-#    print "Columns comprise:\n"
-#    for o in p2.columns: print "{0}: {1}".format(o.cell_index, o.name)
-#    "Destroying project."
-#    p2.destroy()
-#except Exception, e: raise
-#try:
-#    p3=Project(url='http://media.juiceanalytics.com/census/CENSUS_STATEAGESEX.csv', name="census2", **{"separator":","})
-#    print "Columns comprise:\n"
-#    for o in p3._columns: print "{0}: {1}".format(o.cell_index, o.name)
-#    "Project has {0} facets:\n".format(len(p3.facets))
-#    print p3.facets
-#    # Insert facet test here...
-#    range_facet = RangeFacet("AGE", "AGE", 18, 21)
-#    history = p3.history
-#    data = p3.rows(None, 5, 3)
-#    p3.append_facet(range_facet)
-#    comps = p3.compute_facets()
-#    print comps
-#    list_facet = ListFacet("STATE", "STATE", False, False, [{"v":{"v":"Florida","l":"Florida"}}], True, True, False)
-#    history = p3.history
-#    data = p3.rows(None, 5, 3)
-#    p3.append_facet(list_facet)
-#    comps = p3.compute_facets()
-#    print comps
-#    text_facet = TextFacet("SEX", "SEX", "M")
-#    history = p3.history
-#    data = p3.rows(None, 5, 3)
-#    p3.append_facet(text_facet)
-#    comps = p3.compute_facets()
-#    print comps
-#    "Project has {0} facets:\n".format(len(p3.facets))
-#    print "History:\n{0}".format(history)
-#    print "Of {0} total rows {1} were returned after applying facets.".format(data.total_count, data.filtered_count)
-#    print "Sample row:\n{0}\n".format(data.rows[0])
-#    "Project has {0} facets:\n".format(len(p3.facets))
-#    print p3.facets
-#    p3.test_facets([RangeFacet("STATE", "STATE", **{"selectBlank": True, "selectError": True, "expression": "if(endsWith(value, \"%\"), value.replace(\"%\",\"\").replace(\",\",\"\"), \"NaN\").toNumber()", "selectNumeric": True})])
-#    "Destroying project."
-#    p3.destroy()
-#except Exception, e: raise
+
+def testServerUploadColumns():
+    """
+    Testing project creation using media.juiceanalytics.com CSV API...
+    :return:
+    """
+
+    try:
+        s = RefineServer()
+        p = Project(url='http://media.juiceanalytics.com/census/CENSUS_STATEAGESEX.csv', name="census2", separator=",")
+        expecteds = ['STATE', 'SEX', 'AGE', 'POP2000', 'POP2008']
+        for name, c in zip(expecteds, p.columns):
+            assert name == c.name
+    except Exception, e:
+        raise
+    finally:
+        p.destroy()
+
+
+
+
+def testServerFacets():
+    """
+    Testing project creation using media.juiceanalytics.com CSV API...
+    :return:
+    """
+
+    try:
+        s = RefineServer()
+        p = Project(url='http://media.juiceanalytics.com/census/CENSUS_STATEAGESEX.csv', name="census2", separator=",")
+
+        assert len(p.facets) == 0
+
+        # Insert facet test here...
+        range_facet = RangeFacet("AGE", "AGE", 18, 21)
+        data = p.rows(None, 5, 3)
+        p.append_facet(range_facet)
+        comps = p.compute_facets()
+        assert len(p.facets) == 1
+        assert comps[0].__dict__['baseNumericCount'] == 8772
+        assert data.filtered_count == 8772
+
+        list_facet = ListFacet("STATE", "STATE", False, False, [{"v":{"v":"Florida","l":"Florida"}}], True, True, False)
+        data = p.rows(None, 5, 3)
+        p.append_facet(list_facet)
+        comps = p.compute_facets()
+        assert len(p.facets) == 2
+        assert data.filtered_count == 306
+
+        text_facet = TextFacet("SEX", "SEX", "M")
+        history = p.history
+        data = p.rows(None, 5, 3)
+        p.append_facet(text_facet)
+        comps = p.compute_facets()
+        assert data.total_count == 8772
+        assert data.filtered_count == 6
+        assert len(p.facets) == 3
+        p.test_facets([RangeFacet("STATE", "STATE", **{"selectBlank": True, "selectError": True, "expression": "if(endsWith(value, \"%\"), value.replace(\"%\",\"\").replace(\",\",\"\"), \"NaN\").toNumber()", "selectNumeric": True})])
+    except Exception, e:
+        raise
+    finally:
+        p.destroy()
+
+
